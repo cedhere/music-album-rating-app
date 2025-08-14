30aa8ce65637d9a95d92e8bad8d458919a7569b7
Cedric Pierre-Louis <cpierrelouis1114@gmail.com>
2025-08-14 00:54:34 -0400

    fix: remove client as submodule and track as normal folder

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [postsList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((response) => {
+      setPostList(response.data);
+    });
+  }, []);
+  return (
+    <div className="App">
+      {postsList.map((value, key) => { 
+        return <div className="post"> 
+          <div className="user">{value.user_id} rated {value.album_id}</div>
+          <div className="rating">{value.rating}</div> 
+          <div className="caption">{value.caption}</div>
+        </div>
+    })}
+    </div>
+  );
+}
+
+export default App;
